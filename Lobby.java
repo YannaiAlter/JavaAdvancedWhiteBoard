eafbe103af239bc26aa9d002bcd8449893679ff7
Yannai Alter <yanayalter@gmail.com>
2019-07-15 19:30:35 +0300

    Added more classes

---
+++ Lobby.java
@@ -0,0 +1,7 @@
+//Designing the lobby.
+//Target: A Button to join a existing room - then we will get a list of available rooms from RoomManager by RMI!
+//	A Button to create a new room
+class Lobby
+{
+
+}
