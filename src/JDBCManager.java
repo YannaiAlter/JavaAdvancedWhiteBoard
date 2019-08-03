51674e62a3be845cd4951dbcad55f8986357f8a2
Yannai Alter <yanayalter@gmail.com>
2019-08-03 16:07:35 +0300

    Added JDBC Manager

---
+++ src/JDBCManager.java
@@ -0,0 +1,49 @@
+import java.sql.*;
+
+public class JDBCManager {
+    public static void main(String args[]) {
+        try {
+            System.out.println(checkLogin("hello", "admin"));
+        }
+        catch (Exception e) {}
+    }
+
+    public static boolean checkLogin(String username, String password)
+            throws SQLException {
+
+        Connection connection = null; // manages connection
+        PreparedStatement pt = null; // manages prepared statement
+
+        // connect to database usernames and query database
+        try {
+
+            // establish connection to database
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/javaproject", "root", "yanayyanay");
+
+            // query database
+            pt = con.prepareStatement("select username,password from accounts where username=?");
+
+            // process query results
+            pt.setString(1, username);
+            ResultSet rs = pt.executeQuery();
+            String orgUname = "", orPass = "";
+            while (rs.next()) {
+                orgUname = rs.getString("username");
+                orPass = rs.getString("password");
+            } //end while
+            if (orPass.equals(password)) {
+                //do something
+                rs.close();
+                return true;
+
+            } else {
+                //do something
+            }
+        }//end try
+        catch (Exception e) {
+        } //end catch
+        return false;
+    } //end main
+
+}
