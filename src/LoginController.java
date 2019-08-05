d945c8b553aec05fea1cc2bb2546b3ba69d553dd
anuk909 <34924662+anuk909@users.noreply.github.com>
2019-08-05 14:13:24 +0300

    Login Update
    some updates to login and to the design, there is blank check now and password field

--- src/LoginController.java
+++ src/LoginController.java
@@ -1,5 +1,6 @@
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.*;
@@ -11,7 +12,7 @@ public class LoginController {
     TextField username;
 
     @FXML
-    TextField password;
+    PasswordField password;
 
     @FXML
     Label status;
@@ -19,40 +20,51 @@ public class LoginController {
 
     public void onRegisterClick(ActionEvent actionEvent)
     {
-        if(JDBCManager.createUser(username.getText(),password.getText()))
-        {
-            status.setText("Create successfully");
-            status.setVisible(true);
+        try {
+            if (username.getText().isBlank() || password.getText().isBlank()) {
+                status.setText("You must fill both fields");
+                status.setVisible(true);
+                return;
+            }
+            if (JDBCManager.createUser(username.getText(), password.getText())) {
+                status.setText("Account created successfully");
+                status.setVisible(true);
 
+            }
+            else {
+                status.setText("Username is already taken");
+                status.setVisible(true);
+
+            }
         }
-        else
-        {
-            status.setText("Username is already taken");
-            status.setVisible(true);
-
+        catch (Exception e) {
+            System.out.println("An error occurred when connecting to SQL Database");
         }
     }
     public void onLoginMouse(ActionEvent actionEvent) {
         try {
+            if(username.getText().isBlank() || password.getText().isBlank()){
+                status.setText("You must fill both fields");
+                status.setVisible(true);
+                return;
+            }
             if(JDBCManager.checkLogin(username.getText(), password.getText())) {
                 System.out.println("Success"); // Will switch scene
                 status.setText("Success");
                 status.setVisible(true);
+
                 Parent lobbyParent = FXMLLoader.load(getClass().getResource("LobbyDesign.fxml"));
                 Scene lobbyScene = new Scene(lobbyParent);
                 Stage mainStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
                 mainStage.setScene(lobbyScene);
                 mainStage.show();
             }
-            else
-            {
-                status.setText("Please check details");
+            else {
+                status.setText("Unidentified username or invalid password.");
                 status.setVisible(true);
             }
-        }catch (Exception e)
-        {
-
-            System.out.println("An error occured when connecting to SQL Database");
+        }catch (Exception e) {
+            System.out.println("An error occurred when connecting to SQL Database");
         }
 
     }
