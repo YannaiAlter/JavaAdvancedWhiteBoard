240f8289bd3fc13ded8601752f60a493ea72880e
anuk909 <34924662+anuk909@users.noreply.github.com>
2019-08-25 17:04:52 +0300

    Update DBFinals.java
    useSSL=false

--- src/DBFinals.java
+++ src/DBFinals.java
@@ -1,5 +1,5 @@
 public class DBFinals {
-    final static String url="jdbc:mysql://YannaiAlter.com:64158/javaproject";
+    final static String url="jdbc:mysql://YannaiAlter.com:64158/javaproject?useSSL=false";
     final static String user="root";
     final static String password="yanayyanay";
 }
