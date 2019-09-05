c8bf811de0da4ebca2990cb0614eb16c871c9f13
Yannai Alter <yalter@LttsIL.com>
2019-09-05 21:36:16 +0300

    Added DBFinals

---
+++ src/DBFinals.java
@@ -0,0 +1,8 @@
+public class DBFinals {
+
+    final static String url="jdbc:mysql://YannaiAlter.com:38363/javaproject?useSSL=false";
+    final static String user="root";
+    final static String password="yanayyanay";
+    final static String RMIHost = "YannaiAlter.com";
+    final static int RMIPort = 38360;
+}
