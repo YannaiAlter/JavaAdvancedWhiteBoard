5f91d3beefaf144bf2b0c3f02ea2cf8153eff274
Yannai Alter <yanayalter@gmail.com>
2019-09-15 18:00:44 +0300

    added back dbfinals

---
+++ src/DBFinals.java
@@ -0,0 +1,8 @@
+public class DBFinals {
+
+    final static String url="jdbc:mysql://localhost/javaproject?useSSL=false";
+    final static String user="root";
+    final static String password="yanayyanay";
+    final static String RMIHost = "localhost";
+    final static int RMIPort = 1099;
+}
