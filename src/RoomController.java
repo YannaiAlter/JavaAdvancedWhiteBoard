a4b451e4df41be2525cd58072040057a456dfba6
Yannai Alter <yanayalter@gmail.com>
2019-09-15 17:59:00 +0300

    Changed to text area

--- src/RoomController.java
+++ src/RoomController.java
@@ -1,12 +1,13 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class RoomController {
     @FXML
-    TextField outputChat;
+    TextArea outputChat;
     @FXML
     TextField inputChat;
 
